b54ef8f6ca9dda692ba92e0341f009d886fd44e4
chanwooleeme <upup1810@gmail.com>
2022-11-17 18:10:55 +0900

    Initial commit

---
+++ src/test/java/com/classylab/youtubeapi/YoutubeapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.classylab.youtubeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoutubeapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
